31e3c17e15b03086e7c607d8e2b6e8e5c78eee5d
computervisionlearner <33013384+computervisionlearner@users.noreply.github.com>
2018-07-01 10:42:10 +0800

    Update 统计13号出现在周几的次数.cpp

--- 统计13号出现在周几的次数.cpp
+++ 统计13号出现在周几的次数.cpp
@@ -25,9 +25,10 @@ int get_days(int y, int m)
 	return 30;
 }
 
-int main()
+int count()
 {
-	//第一个13号是星期六；注意题目条件，有的需要判断输入是否异常
+	//第一个13号是星期六
+	//这个函数是用于计算13号分布在星期几。。
 	int n, a[7] = { 0 }, week = 6; 
 	cin >> n;
 
@@ -45,5 +46,43 @@ int main()
 		cout << a[k] << "  ";
 	}
 	cout << endl;
+	return 0;
+}
+
+int count1()
+{
+	//这个函数是计算从1900~1900+n-1年中，所有星期五是哪一号的数量
+
+	int a[32] = { 0 };
+	int N;
+	cin >> N;
+	int days;
+	int end = 0;
+	for (int year = 1900;year <= 1900 + N - 1;year++)
+	{		
+		for (int month = 1;month <= 12;month++)
+		{
+			days = get_days(year, month);			
+			int weeks;
+			for (int k = 1;k <= days;k++)
+			{
+				weeks = (end + k) % 7;
+				if (weeks == 5)
+				{
+					a[k]++;
+				}
+			}
+			end = weeks;
+		}
+	}
+	cout << a[13] << endl;
+	return 0;
+}
+
+
+void main()
+{
+	count();
+	count1();
 	system("pause");
 }
